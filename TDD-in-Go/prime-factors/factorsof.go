4f8981cdae98ea6f3cb716a37e2a853ef1fa6abf
tanzhuxiaqiu <tanzhuxiaqiu@huawei.com>
2018-10-18 11:20:22 +0800

    done with factorsof.go

--- TDD-in-Go/prime-factors/factorsof.go
+++ TDD-in-Go/prime-factors/factorsof.go
@@ -2,8 +2,13 @@ package primefactors
 
 func factorsOf(n int) []int {
 	var factors []int
-	if n > 1 {
-		return append(factors, 2)
+
+	for divisor := 2; n > 1; divisor++ {
+		for n % divisor == 0 {
+			factors = append(factors, divisor)
+			n /= divisor
+		}
 	}
+
 	return factors
 }
