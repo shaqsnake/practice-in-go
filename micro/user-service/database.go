1b6adf68067af4296a5bc329b2fc806e5ed44921
shaqsnake <shaqsnake@gmail.com>
2019-10-28 17:38:48 +0800

    Fixed user-service db connecting.

--- micro/user-service/database.go
+++ micro/user-service/database.go
@@ -8,18 +8,15 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+// CreateConnection -
 func CreateConnection() (*gorm.DB, error) {
 	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
 	dbname := os.Getenv("DB_NAME")
 	password := os.Getenv("DB_PASSWORD")
-	fmt.Println(host, dbname, user, password)
+	
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", host, user, dbname, password) //Build connection string
+	fmt.Println(dbURI)
 
-	return gorm.Open(
-		"postgres",
-		fmt.Sprintf(
-			"host=%s user=%s dbname=%s sslmode=disable password=%s",
-			host, user, dbname, password,
-		),
-	)
+	return gorm.Open("postgres", dbURI)
 }
